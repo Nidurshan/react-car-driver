7a05652f0177ca3e8ed4dbae621f330c5a57c409
Nidurshan <nidurshanpro@gmail.com>
2024-01-19 21:32:49 +0530

    Change the title "React Car Game" to "React Car Driver"

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className='header'>
-        <h1 className='title'><span className='title-1'>React</span> Car Game</h1>
+        <h1 className='title'><span className='title-1'>React</span> Car Driver</h1>
     </div>
 
   )
