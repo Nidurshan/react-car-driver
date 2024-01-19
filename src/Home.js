6508360615f795a4cbd01793cda392999eedae84
Nidurshan <nidurshanpro@gmail.com>
2024-01-19 21:27:56 +0530

    Create car controls to move and turn

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,85 @@
 import React, { useState } from 'react';
 import "./Home.css";
-import './mycar.png';
+import myCarImage from './mycar.png';
 
 const Home = () => {
 
-    const getCar = document.getElementById("car");
-    const [car, setCar] = useState(getCar);
+    const [carPosition, setCarPosition] = useState({ top: 0, left: 0 });
+    const [carDirection, setCarDirection] = useState("N");
+    const [rotation, setRotation] = useState(0);
+    const step = 10;
 
-    function moveUp() {
-
+    var drivePositions = {
+        N: { ...carPosition, top: carPosition.top - step },
+        E: { ...carPosition, left: carPosition.left + step },
+        S: { ...carPosition, top: carPosition.top + step },
+        W: { ...carPosition, left: carPosition.left - step },
     }
 
-  return (
-    <div>
+    var reversePositions = {
+        N: { ...carPosition, top: carPosition.top + step },
+        E: { ...carPosition, left: carPosition.left - step },
+        S: { ...carPosition, top: carPosition.top - step },
+        W: { ...carPosition, left: carPosition.left + step },
+    }
+
+    const leftTurns = {
+        N: "W",
+        E: "N",
+        S: "E",
+        W: "S",
+    }
+
+    const rightTurns = {
+        N: "E",
+        E: "S",
+        S: "W",
+        W: "N",
+    }
+
+    function move(control){
+        switch (control) {
+            case "drive":
+                setCarPosition(drivePositions[carDirection]);
+                break;                
+            case "reverse":                
+                setCarPosition(reversePositions[carDirection]);
+                break;
+        }
+    }
+
+    function turn(control){
+        switch (control) {
+            case "left":
+                setCarDirection(leftTurns[carDirection]);
+                setRotation(rotation - 90);
+                break;                
+            case "right":
+                setCarDirection(rightTurns[carDirection]);
+                setRotation(rotation + 90);
+                break;
+        }
+    }
+
+    return (
         <div>
-            <button className="arrowBtn">Up</button>
-            <br />
-            <button className="arrowBtn">Down</button>
-            <br />
-            <button className="arrowBtn">Left</button>
-            <br />
-            <button className="arrowBtn">Right</button>
-        </div>
-        <div className='playground'>
-            <div className='car-Container'>
-                <img src="./mycar.png" alt="" id='car' />
+            <div>
+                <button className="arrowBtn" onClick={() => move('drive')}>Drive</button>
+                <br />
+                <button className="arrowBtn" onClick={() => move('reverse')}>Reverse</button>
+                <br />
+                <button className="arrowBtn" onClick={() => turn('left')}>Left</button>
+                <br />
+                <button className="arrowBtn" onClick={() => turn('right')}>Right</button>
+            </div>
+            <div className='playground'>
+                <div className='car-Container' style={{ ...carPosition,
+                    backgroundImage: `url(${myCarImage})`, 
+                    transform: `rotate(${rotation}deg)`
+                }}></div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default Home;
